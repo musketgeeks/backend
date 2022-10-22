380459214fc9d5679f15930d51bfbe86dcb3358d
Daniel Sousa <d.sousa@kigroup.de>
2022-10-22 10:01:50 +0100

    fix: eslint config file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,86 +24,84 @@ module.exports = {
 		node: true,
 		jest: true
 	},
-	extends: ['airbnb-base', 'prettier'],
-	plugins: ['prettier'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		sourceType: 'module',
+	},
+	plugins: [
+		'@typescript-eslint/eslint-plugin',
+		'simple-import-sort',
+		'import',
+		'unused-imports',
+		'prettier',
+	],
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+		'prettier',
+	],
 	rules: {
+		curly: 'error',
+		'no-console': 'error',
+		'@typescript-eslint/interface-name-prefix': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-unused-vars': noUnusedVarRule,
+		'unused-imports/no-unused-vars': noUnusedVarRule,
+		'@typescript-eslint/naming-convention': [
+			'error',
+			{
+				selector: 'variableLike',
+				format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+				leadingUnderscore: 'allow'
+			}
+		],
+		'@typescript-eslint/indent': 'off',
+		'import/prefer-default-export': 'off',
+		'import/no-cycle': 'off',
+		'simple-import-sort/exports': 'error',
+		'import/first': 'error',
+		'import/newline-after-import': 'error',
+		'import/no-duplicates': 'error',
+		'unused-imports/no-unused-imports': 'error',
+		'simple-import-sort/imports': [
+			'error',
+			{
+				groups: [
+					[
+						'^@nestjs',
+						'^@nestjs\\/([a-z0-9]+)',
+						'^nestjs',
+						'^nestjs\\/([a-z0-9]+)',
+						'^nestjs-([a-z0-9]+)',
+						'^jest\\/([a-z0-9]+)',
+						'^jest-([a-z0-9]+)',
+						'^@?\\w'
+					],
+					[
+						`^(${foldersPaths.join('|')})(/.*|$)`,
+						`^(${folders.join('|')})(/.*|$)`
+					],
+					['^\\.', '^']
+				]
+			}
+		],
 		'prettier/prettier': ['error', { usePrettierrc: true }]
 	},
 	overrides: [
 		{
-			// Rules for TypeScript
-			files: ['*.ts', '*.tsx'],
-			extends: [
-				'plugin:@typescript-eslint/recommended',
-				'airbnb-base',
-				'airbnb-typescript/base'
-			],
-			plugins: [
-				'@typescript-eslint/eslint-plugin',
-				'simple-import-sort',
-				'import',
-				'unused-imports'
-			],
-			excludedFiles: ['*.js', '*.cjs', '*.mjs'],
-			parserOptions: {
-				project: 'tsconfig.json',
-				tsconfigRootDir: __dirname,
-				sourceType: 'module'
-			},
-			rules: {
-				curly: 'error',
-				'no-console': 'error',
-				'@typescript-eslint/interface-name-prefix': 'off',
-				'@typescript-eslint/explicit-function-return-type': 'off',
-				'@typescript-eslint/explicit-module-boundary-types': 'off',
-				'@typescript-eslint/no-explicit-any': 'off',
-				'@typescript-eslint/no-unused-vars': noUnusedVarRule,
-				'unused-imports/no-unused-vars': noUnusedVarRule,
-				'@typescript-eslint/naming-convention': [
-					'error',
-					{
-						selector: 'variableLike',
-						format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
-						leadingUnderscore: 'allow'
-					}
-				],
-				'@typescript-eslint/indent': 'off',
-				'import/prefer-default-export': 'off',
-				'import/no-cycle': 'off',
-				'simple-import-sort/exports': 'error',
-				'import/first': 'error',
-				'import/newline-after-import': 'error',
-				'import/no-duplicates': 'error',
-				'unused-imports/no-unused-imports': 'error',
-				'simple-import-sort/imports': [
-					'error',
-					{
-						groups: [
-							[
-								'^@nestjs',
-								'^@nestjs\\/([a-z0-9]+)',
-								'^nestjs',
-								'^nestjs\\/([a-z0-9]+)',
-								'^nestjs-([a-z0-9]+)',
-								'^jest\\/([a-z0-9]+)',
-								'^jest-([a-z0-9]+)',
-								'^@?\\w'
-							],
-							[
-								`^(${foldersPaths.join('|')})(/.*|$)`,
-								`^(${folders.join('|')})(/.*|$)`
-							],
-							['^\\.', '^']
-						]
-					}
-				]
-			}
-		},
-		{
 			// For config files
-			files: ['.eslintrc.cjs', 'commitlint.config.cjs', '*.cjs'],
+			files: ['*.cjs', '*.mjs', '*.js'],
 			env: {
 				node: true
+			},
+			extends: ['prettier'],
+			plugins: ['prettier'],
+			rules: {
+				'import/no-extraneous-dependencies': 'off',
+				'prettier/prettier': ['error', { usePrettierrc: true }]
 			}
 		}
 	]
--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -1,13 +1,18 @@
 ## Description
+
 <!-- Description about what you do in this pull request -->
 
 ### Checklist
-- [ ] Task 1
-- [ ] Task 2
+
+-   [ ] Task 1
+-   [ ] Task 2
 
 ## Notes
+
 <!-- If you have notes to add -->
 
 ## References
+
 <!-- Change <X> for the issue number -->
+
 Closes [BACKEND-<X>](https://musketgeeks.youtrack.cloud/issue/BACKEND-<X>/)
