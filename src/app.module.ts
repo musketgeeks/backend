c31502d6ce62e2b66864c0c5cc71b41e1d4d0d74
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2022-10-29 21:02:33 +0100

    feat: update .env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 
-import { AppConfigModule } from '@app/config/app/config.module';
-import { AppConfigService } from '@app/config/app/config.service';
+import { AppConfigModule } from '@config/app/config.module';
+import { AppConfigService } from '@config/app/config.service';
+import { DatabaseProviderModule } from '@providers/database/provider.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-	imports: [AppConfigModule],
+	imports: [AppConfigModule, DatabaseProviderModule],
 	controllers: [AppController],
 	providers: [AppService, AppConfigService]
 })
