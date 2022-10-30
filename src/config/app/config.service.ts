b7a18fd865bc78efe272c088b1ce856f8df3e2f1
Daniel Sousa <d.sousa@kigroup.de>
2022-10-30 16:13:32 +0000

    fix: type of app port

--- src/config/app/config.service.ts
+++ src/config/app/config.service.ts
@@ -6,6 +6,6 @@ export class AppConfigService {
 	constructor(private configService: ConfigService) {}
 
 	get port() {
-		return this.configService.get<string>('app.port');
+		return this.configService.get<number>('app.port');
 	}
 }
