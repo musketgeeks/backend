ca5fe72a227a4f188a43ad41a228f7f7cd32add5
Daniel Sousa <d.sousa@kigroup.de>
2022-10-31 21:17:00 +0000

    fix: typeorm problems

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,55 @@
+import { genSalt, hash } from 'bcrypt';
+import { Exclude } from 'class-transformer';
+import {
+	BaseEntity,
+	BeforeInsert,
+	BeforeUpdate,
+	Column,
+	CreateDateColumn,
+	Entity,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn
+} from 'typeorm';
+
+import { Role } from '@modules/users/enums/Role';
+
+@Entity('users')
+export class User extends BaseEntity {
+	@PrimaryGeneratedColumn('uuid')
+	readonly id: string;
+
+	@Column()
+	name: string;
+
+	@Column({ unique: true })
+	email: string;
+
+	@Column({ unique: true })
+	username: string;
+
+	@Column()
+	@Exclude()
+	password: string;
+
+	@Column({ nullable: true })
+	avatar: string;
+
+	@Column({ enum: Role })
+	role: string;
+
+	@CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+	createdAt: Date;
+
+	@UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+	updatedAt: Date;
+
+	@BeforeInsert()
+	@BeforeUpdate()
+	private async generateHashPassword() {
+		if (this.password) {
+			// Create env for salt
+			const salt = await genSalt();
+			this.password = await hash(this.password, salt);
+		}
+	}
+}
