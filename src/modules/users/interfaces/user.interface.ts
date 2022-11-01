2f41bc20d06466a90120eb853825136159deaece
Daniel Sousa <d.sousa@kigroup.de>
2022-11-01 16:49:19 +0000

    fix: paths on imports

--- src/modules/users/interfaces/user.interface.ts
+++ src/modules/users/interfaces/user.interface.ts
@@ -1,5 +1,5 @@
+import { Role } from '@common/enums/Role';
 import { TimestampsInterface } from '@common/interfaces/timestamps.interface';
-import { Role } from '@modules/users/enums/Role';
 
 export interface UserInterface extends TimestampsInterface {
 	readonly id?: string; // optional because only have after creation
