4bc6daa8ceb601e215c088a59114f00dc7330972
Francisco Morgado <f.morgado@kigroup.de>
2022-11-04 20:41:18 +0000

    feat: dto for travel and place, updated category dto

--- src/modules/places/dto/create-place.dto.ts
+++ src/modules/places/dto/create-place.dto.ts
@@ -1,1 +1,28 @@
-export class CreatePlaceDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePlaceDto {
+	@IsNotEmpty()
+	@IsString()
+	readonly name: string;
+
+	@IsNotEmpty()
+	@IsString()
+	readonly slug: string;
+
+	@IsString()
+	readonly description: string;
+
+	@IsString()
+	readonly lat: string;
+
+	@IsString()
+	readonly log: string;
+
+	@IsString()
+	readonly city: string;
+
+	@IsString()
+	@IsNotEmpty()
+	// TODO this is not empty needs to be discussed
+	readonly country: string;
+}
--- src/modules/travels/dto/create-travel.dto.ts
+++ src/modules/travels/dto/create-travel.dto.ts
@@ -1,1 +1,38 @@
-export class CreateTravelDto {}
+import {
+	IsEnum,
+	IsNotEmpty,
+	IsNumber,
+	IsOptional,
+	IsString
+} from 'class-validator';
+
+import { Transportation } from '@common/enums/Transportation';
+
+export class CreateTravelDto {
+	//TODO need to add the user_id connection??
+
+	@IsNotEmpty()
+	@IsString()
+	readonly name: string;
+
+	@IsNumber()
+	@IsNotEmpty()
+	// TODO needs to be discussed due to being empty or not
+	readonly duration: number;
+
+	@IsString()
+	@IsOptional()
+	readonly begins: string;
+
+	@IsString()
+	@IsOptional()
+	readonly city: string;
+
+	@IsString()
+	@IsNotEmpty()
+	readonly country: string;
+
+	@IsEnum(Transportation)
+	@IsNotEmpty()
+	readonly transportation: Transportation;
+}
