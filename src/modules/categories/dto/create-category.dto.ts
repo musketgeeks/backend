c4d10ed0021777a6642e2e71e7d528fe4f6ac758
Francisco Morgado <f.morgado@kigroup.de>
2022-11-04 18:25:38 +0000

    fix: modules on the respective folder and category dto

--- src/modules/categories/dto/create-category.dto.ts
+++ src/modules/categories/dto/create-category.dto.ts
@@ -1,11 +1,15 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCategoryDto {
-	@IsString()
+
 	@IsNotEmpty()
+	@IsString()
+
 	readonly name: string;
 
-	@IsString()
 	@IsNotEmpty()
+	@IsString()
+
 	readonly description: string;
 }
+
