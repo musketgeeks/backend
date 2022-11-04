70053d4add1e8b70496bbfb2efe7c108dfeca97a
Catia Barroco <104831678+CatiaBarroco-xgeeks@users.noreply.github.com>
2022-11-04 17:54:58 +0000

    feat: add modules on backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,14 +8,13 @@ import { DatabaseProviderModule } from '@providers/database/provider.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ModuleModule } from './modules/places/module/module.module';
 import { CategoriesModule } from './modules/categories/categories/categories.module';
 import { PlacesModule } from './modules/places/places/places.module';
 import { TravelsModule } from './modules/travels/travels/travels.module';
 
 
 @Module({
-	imports: [AppConfigModule, DatabaseProviderModule, AuthModule, UsersModule, ModuleModule, CategoriesModule, PlacesModule, TravelsModule],
+	imports: [AppConfigModule, DatabaseProviderModule, AuthModule, UsersModule, CategoriesModule, PlacesModule, TravelsModule],
 	controllers: [AppController],
 	providers: [AppService, AppConfigService]
 })
